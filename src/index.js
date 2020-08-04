06cfaac82a638ea72ce3258c7bbf2c75d48e0fd2
Emiliano Bonassi <bonassi.emiliano@gmail.com>
2020-08-04 14:07:51 +0200

    Update MultiSend to MultiSendPayable in Rinkeby

--- src/index.js
+++ src/index.js
@@ -107,7 +107,7 @@ const defaultNetworks = {
   4: {
     masterCopyAddress: '0x6851d6fdfafd08c0295c392436245e5bc78b0185',
     proxyFactoryAddress: '0x336c19296d3989e9e0c2561ef21c964068657c38',
-    multiSendAddress: '0xB522a9f781924eD250A11C54105E51840B138AdD',
+    multiSendAddress: '0x636D3336B120498CC3B596FF30B20e87d813953f',
     fallbackHandlerAddress: '0x40A930851BD2e590Bd5A5C981b436de25742E980',
   },
   // goerli
